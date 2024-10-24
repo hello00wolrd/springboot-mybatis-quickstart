ee854cacd186c61a19497a6429937a5516c9d96a
jianingDai <752442633@qq.com>
2024-10-24 16:54:37 +0800

    Initial commit

---
+++ src/test/java/whu/edu/SpringbootMybatisQuickstartApplicationTests.java
@@ -0,0 +1,13 @@
+package whu.edu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMybatisQuickstartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
